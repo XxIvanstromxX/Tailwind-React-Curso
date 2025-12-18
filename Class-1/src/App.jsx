8ee2b6fa2bbb07dfc52b15807a8ddcbcd7fb3485
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-12-17 22:28:15 -0600

    Agregar componente App con estructura básica y Navbar

---
+++ Class-1/src/App.jsx
@@ -0,0 +1,32 @@
+import { Navbar } from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      {/* <div className="p-4 bg-amber-200 rounded-4xl shadow flex justify-center items-center gap-5 flex-col">
+        <h1 className="text-gray-500 text-2xl md:4xl font-bold">
+          Hola mundo
+        </h1>
+        <h1 className="text-red-500 text-2xl md:4xl font-bold">
+          desde react
+        </h1>
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-4 bg-amber-100 h-100 p-4 rounded-4xl shadow">
+        <div className="bg-blue-700"></div>
+        <div className="bg-red-700"></div>
+        <div className="bg-green-700"></div>
+        <div className="bg-yellow-700"></div>
+      </div> */}
+    </>
+  )
+}
+
+export default App
+
+// Sistema de espaciado 
+// padding -> p- px- py- pt- pb- pl- pr-
+// margin -> m- mx- my- mt- mb- ml- mr-
+// gap -> gap-
