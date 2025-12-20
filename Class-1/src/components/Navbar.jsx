922e9dd2ea0d6fb220a251fd1ba010858099405c
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-12-20 12:40:59 -0600

    Agregar espacios en blanco para mejorar la legibilidad del componente Navbar

--- Class-1/src/components/Navbar.jsx
+++ Class-1/src/components/Navbar.jsx
@@ -4,11 +4,13 @@ export function Navbar(){
             <h1 className="text-xl font-bold">
                 MiMarca
             </h1>
+            
             <div className="hidden md:flex gap-6">
                 <a href="#" className="hover:text-red-600">Inicio</a>
                 <a href="#" className="hover:text-red-600">Servicios</a>
                 <a href="#" className="hover:text-red-600">Contacto</a>
             </div>
+
             <button className="bg-red-600 text-white px-4 py-2 rounded hover:bg-indigo-800 transition">
                 Empezar
             </button>
