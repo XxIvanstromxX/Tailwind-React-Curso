8023a0d4002de663220cb871897f0d0caf29ad3f
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-12-23 18:01:34 -0600

    Agregar componente App con NavBar y LandingPage

---
+++ Class-3/src/App.jsx
@@ -0,0 +1,14 @@
+import { NavBar } from "./components/NavBar"
+import LandingPage from "./pages/LandingPage"
+
+function App() {
+
+  return (
+    <div className="bg-radial-[at_50%_1000%] from-amber-600 via-amber-600 to-amber-50 to-95% dark:bg-radial-[at_50%_1000%] dark:from-gray-400 dark:via-gray-600 dark:to-gray-900 min-h-screen transition-colors duration-500">
+      <NavBar />
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
