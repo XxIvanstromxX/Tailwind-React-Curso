a209ef948017f1c9dede04783e26b420605adcee
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-12-23 21:59:07 -0600

    Agregar componente Footer en el componente App

--- Class-3/src/App.jsx
+++ Class-3/src/App.jsx
@@ -1,5 +1,6 @@
 import { NavBar } from "./components/NavBar"
 import LandingPage from "./pages/LandingPage"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div className="bg-radial-[at_50%_1000%] from-amber-600 via-amber-600 to-amber-50 to-95% dark:bg-radial-[at_50%_1000%] dark:from-gray-400 dark:via-gray-600 dark:to-gray-900 min-h-screen transition-colors duration-500">
       <NavBar />
       <LandingPage />
+      <Footer />
     </div>
   )
 }
